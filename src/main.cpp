426a76af3669d6e4590455302f89f9db1390c946
t7cel <bachinalva@gmail.com>
2025-06-06 00:33:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,46 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <fstream>
+#include <algorithm>
 #include "tree.h"
 
-void print_vector(const std::vector<char>& vec) {
-    for (char c : vec) {
-        std::cout << c << " ";
+int main() {
+    std::vector<char> input1 = {'1', '3', '5', '7'};
+    PMTree tree1(input1);
+    std::vector<char> result1 = getPerm1(tree1, 1);
+    std::vector<char> result2 = getPerm2(tree1, 1);
+
+    std::cout << "Permutation 1 (getPerm1): ";
+    for (char c : result1) {
+        std::cout << c;
     }
     std::cout << std::endl;
-}
 
-
-int main() {
-  std::cout << "Примеры использования:" << std::endl;
-    PMTree::char_vector input = {'1', '2', '3'};
-    PMTree tree(input);
-
-    std::cout << "Все перестановки:" << std::endl;
-    std::vector<PMTree::char_vector> allPerms = tree.getAllPerms();
-    for (const auto& perm : allPerms) {
-        print_vector(perm);
+     std::cout << "Permutation 1 (getPerm2): ";
+    for (char c : result2) {
+        std::cout << c;
     }
+    std::cout << std::endl;
 
-    std::cout << "Перестановка №1 (getPerm1): ";
-    print_vector(tree.getPerm1(1));
-    std::cout << "Перестановка №4 (getPerm1): ";
-    print_vector(tree.getPerm1(4));
 
-    std::cout << "Перестановка №1 (getPerm2): ";
-    print_vector(tree.getPerm2(1));
-    std::cout << "Перестановка №4 (getPerm2): ";
-    print_vector(tree.getPerm2(4));
+    std::vector<char> input2 = {'1', '5', '7', '5'};
+    PMTree tree2(input2);
+    std::vector<char> result3 = getPerm1(tree2, 2);
+    std::vector<char> result4 = getPerm2(tree2, 2);
+
+    std::cout << "Permutation 2 (getPerm1): ";
+    for (char c : result3) {
+        std::cout << c;
+    }
+    std::cout << std::endl;
+
+     std::cout << "Permutation 2 (getPerm2): ";
+    for (char c : result4) {
+        std::cout << c;
+    }
+    std::cout << std::endl;
   
   return 0;
 }
